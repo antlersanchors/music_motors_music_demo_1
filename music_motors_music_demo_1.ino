881b6050039d0a99e9137ece9a428c592dddca26
Michael Liston <michaelowen.liston@gmail.com>
2015-04-21 11:43:59 +0200

    Setting frequency using velocity

--- music_motors_music_demo_1.ino
+++ music_motors_music_demo_1.ino
@@ -9,48 +9,64 @@ int c = 0;
 int last_pos = 0;
 
 byte waveFormArray[] = { SINE,
-                        SQUARE,
-                        PULSE,
-                        TRIANGLE,
-                        SAW,
-                        FUZZ,
-                        /* DIGI1,
-                        DIGI2,
-                        DIGI3,
-                        DIGI4,
-                        NOISE,
-                        DIGI6,
-                        TAN1,
-                        TAN2,
-                        TAN3,
-                        TAN4 */
+                         SQUARE,
+                         PULSE,
+                         TRIANGLE,
+                         SAW,
+                         FUZZ,
+                         /* DIGI1,
+                         DIGI2,
+                         DIGI3,
+                         DIGI4,
+                         NOISE,
+                         DIGI6,
+                         TAN1,
+                         TAN2,
+                         TAN3,
+                         TAN4 */
                        };
 
 void setup() {
-  
+
   tick = millis();
   last_pos = analogRead(A1);
-  
+
   Music.init();
-  
+
   Music.setWaveform(SINE);
 
 }
 
 void loop() {
-  
+
   long tick_now = millis();
-  int current_pos = analogRead(A1);
-  
-  
+
+  long dt = tick_now - tick;
+
+  if (dt >= 20) {
+
+    c++;
+
+    int current_pos = analogRead(A1);
+    int dx = last_pos - current_pos;
+    float vel = (float) dx / (float) dt;
+
+    if (c % 15 == 0) {
+      Serial.println(vel);
+    }
+
+    tick = millis();
+    last_pos = current_pos;
+
+  }
+
   int posA = analogRead(A1);
   int posB = analogRead(A9);
-  
+
   int freqA = map(posA, 0, 1023, 500, 1500);
   int freqB = map(posB, 0, 1023, 500, 1500);
-  
-  Music.setFrequency1((float)freqA); 
-  Music.setFrequency2((float)freqB);
 
+  Music.setFrequency1((float)freqA);
+  Music.setFrequency2((float)freqB);
 
 }
