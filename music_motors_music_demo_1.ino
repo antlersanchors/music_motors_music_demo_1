9e59c84f87014e147a7ee878e550b3b1b12b6568
Michael Liston <michaelowen.liston@gmail.com>
2015-04-21 10:28:26 +0200

    Bringing in some new waveforms

--- music_motors_music_demo_1.ino
+++ music_motors_music_demo_1.ino
@@ -3,19 +3,39 @@
 #include <EEPROM.h>
 #include <M3T3.h>
 
+byte waveFormArray[] = { SINE,
+                        SQUARE,
+                        PULSE,
+                        TRIANGLE,
+                        SAW,
+                        FUZZ,
+                        DIGI1,
+                        DIGI2,
+                        DIGI3,
+                        DIGI4,
+                        NOISE,
+                        DIGI6,
+                        TAN1,
+                        TAN2,
+                        TAN3,
+                        TAN4
+                       };
+
 void setup() {
   Music.init();
 
 }
 
 void loop() {
-  init posA = analogRead(A1);
-  init posB = analogRead(A9);
+  int posA = analogRead(A1);
+  int posB = analogRead(A9);
   
-  init freq = map(posA, 0, 1023, 500, 1500);
-  init freq = map(posB, 0, 1023, 500, 1500);
+  int freq = map(posA, 0, 1023, 500, 1500);
+  int freq = map(posB, 0, 1023, 500, 1500);
   
-  Music.setFrequency1(freqA);
+  Music.setFrequency1(freqA); 
   Music.setFrequency2(freqB);
+  
+  Music.setGain() // this one takes a float value between 0-1.0
 
 }
